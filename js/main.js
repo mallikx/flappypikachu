3301bde7d4f04d1001b365899f270e9c3c88493f
Malik Jackson <secondary.malik@gmail.com>
2014-08-14 17:02:14 +0000

    init

---
+++ js/main.js
@@ -0,0 +1,55 @@
+//Initialize Phaser Engine. Create a 400x490px game!
+
+var game = new Phaser.Game(400,490, Phaser.AUTO, 'gameDiv');
+
+//Create our 'main' state that will contain the game
+//This is the body of the game itself
+//It should contain all code related to the game itself
+
+var mainState = { 
+  preload: function () { 
+   //This fuction will execute at the beginning 
+   //Which is where we'll load our assets for the game
+     
+   //Set the background color of the game
+   game.stage.backgroundColor = "#B01C48";
+   
+   game.load.image('bird', 'assets/50px-SSB4_Pikachu.png');
+   
+   game.load.image('pipe', 'assets/pipe.png');     
+  },
+    create: function () {
+      //This function is called right after preload function
+      //This is where we set up the game assets from earlier
+      
+      game.physics.startSystem(Phaser.Physics.ARCADE);
+      
+      this.bird = this.game.add.sprite(100,245, 'bird');
+      
+      game.physics.arcade.enable(this.bird);
+      
+      this.bird.body.gravity.y = 1000;
+      
+      var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+      
+      spaceKey.onDown.add(this.jump, this);   
+   },
+      update: function () {
+      //This function runs 60 times per second
+      //Check if the bird is outside the game screen
+      
+      if(this.bird.inWorld == false) {
+        this.restartGame();
+} 
+},
+    jump: function () {
+      this.bird.body.velocity.y = -350;      
+},           
+    restartGame: function () {
+    game.state.start('main');
+},
+ };
+ 
+//Add and start the 'mainState' to start the game
+game.state.add('main', mainState);
+game.state.start('main');
